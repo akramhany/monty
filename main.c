a214c9ca3263ae125fe1f8c40ed0191982814978
akramhany <akramhany65@gmail.com>
2024-02-07 17:32:10 +0200

    feat: Create the main.c file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Handle arguments, monty file and instructions execution.
+ * @argc: number of input arguments.
+ * @argv: list of input strings.
+ * Return: number to indicate if the program failed or succeded.
+ */
+int main(int argc, char *argv[])
+{
+
+}
