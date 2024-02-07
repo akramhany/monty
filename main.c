06bb4a98202b7e8191246b90f8718c0c0a2f15c3
akramhany <akramhany65@gmail.com>
2024-02-07 22:19:35 +0200

    feat: Implement the push and pall instruction inside

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "main.h"
 
+instruction_t push;
+instruction_t pall;
 
 /**
  * main - Handle arguments, monty file and instructions execution.
@@ -10,7 +14,11 @@
  */
 int main(int argc, char *argv[])
 {
+	stack_t* stack = NULL;
 	FILE* montyFile = NULL;
+	char buffer[1024];
+	char* instruction = NULL;
+	int line_number = 1;
 
 	/*If number of passed arguments not equal 2 print an error and exit*/
 	if (argc != 2)
@@ -29,7 +37,36 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	/*Initialize all existing instructions*/
+	push.f = add_dnodeint;
+	pall.f = printAll;
+
+	while (fgets(buffer, sizeof(buffer), montyFile) != NULL)
+	{
+		instruction = strtok(buffer, " ");
+
+		trim_dSign(instruction);
+
+		if (strcmp(instruction, "push") == 0)
+		{	
+			push.opcode = strtok(NULL, " ");
+			trim_dSign(push.opcode);
+			push.f(&stack, line_number);
+		}
+		else if (strcmp(instruction, "pall") == 0)
+		{
+			pall.f(&stack, line_number);
+		}
+		else if (strcmp(instruction, "$") != 0)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, instruction);
+		}
+
+		line_number++;
+	}
+
 	fclose(montyFile);
+	free_dlistint(stack);
 
 	return (0);
 }
